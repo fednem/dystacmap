ec195df0c4e47208f883b7b2b8db2c6355223674
fednem <federico.nemmi@gmail.com>
2017-11-10 11:09:41 +0100

    modified the source file of qa variable qa_variables_all.txt and modified accordingly qa_comparison.s. Note that now the Anova uses a type II SOS, since the design is unbalanced

--- qa_comparison.s
+++ qa_comparison.s
@@ -6,15 +6,30 @@ qa_variables <- read_delim("QA_covariates_all.txt", delim = " ") %>%
          group = c(rep("group_1",21), rep("group_2",11), rep("group_3", 25), rep("group_4",15),rep("group_5",21),
                    rep("group_1",22), rep("group_2",19), rep("group_3", 14), rep("group_4",20),rep("group_5",20)))
 
+qa_variables <- read_delim("QA_covariates_all.txt", delim = " ") %>%
+  mutate(centre = ifelse(centre_1a == 0,ifelse(centre_1b == 1, "toulouse_upgrade", "aix-marseille"),"toulouse"), 
+         group = if_else(group_1 == 0, 
+                         (if_else(group_2 == 0, 
+                                 if_else(group_3 == 0, 
+                                         if_else (group_4 == 0, "group_5", "group_4"),
+                                 "group_3"), 
+                          "group_2")), 
+                  "group_1")) %>%
+  filter(!is.na(exclusion_vector)) %>%
+  select(-(centre_1a:group_5))
+
+
 qa_variables_centre_by_group.anova <- qa_variables %>%
   gather(., measure, values, -centre, -group) %>%
   split(., .$measure) %>%
   map(~lm(data = ., values ~ centre*group)) %>%
-  map(~Anova(mod = ., t = 3))
+  map(~Anova(mod = ., t = 2))
 
-qa_measure_with_main_effect_centre <- c("QA_GCOR_rest", "QA_InvalidScans", "QA_MaxGlobal")
-qa_measure_with_main_effect_group <- c("QA_GCOR_rest", "QA_WhiteMatter_eroded_vol", "QA_WhiteMatter_vol")
-qa_measure_with_interaction <- c("QA_CSF_eroded_vol", "QA_CSF_vol", "QA_MaxMotion")
+qa_measure_with_main_effect_centre <- c("QA_CSF_eroded_vol", "QA_CSF_vol", "QA_InvalidScans",
+                                        "QA_MaxGlobal", "QA_MeanGlobal", "QA_MeanMotion", "QA_WhiteMatter_eroded_vol")
+qa_measure_with_main_effect_group <- c("QA_GCOR_rest", "QA_GreyMatter_eroded_vol", "QA_GreyMatter_vol",
+                                       "QA_WhiteMatter_vol", "QA_WhiteMatter_eroded_vol")
+
 
 barplot_main_effect_centre <- qa_variables %>%
   gather(., measure, values, -centre, -group) %>%
@@ -50,20 +65,3 @@ pdf("qa_with_main_effect_of_group.pdf", w = 5, h = 4)
 print(barplot_main_effect_group)
 dev.off()
 
-barplot_interaction <- qa_variables %>%
-  gather(., measure, values, -centre, -group) %>%
-  split(., .$measure) %>%
-  `[`(., names(.) %in% qa_measure_with_interaction) %>%
-  map(~group_by(.,centre,group) %>%
-        summarise(avg = mean(values), lower_ci = t.test(values)$conf.int[1], upper_ci = t.test(values)$conf.int[2])) %>%
-  map2(.x = ., qa_measure_with_interaction, ~ggplot(data = .x, aes(x = interaction(centre,group), y = avg, fill = centre)) + 
-         geom_bar(stat = "identity", position = position_dodge()) + 
-         geom_errorbar(aes(ymin = lower_ci, ymax = upper_ci), position = "dodge", width = .2) + 
-         ggtitle(.y) + 
-         theme_bw() + 
-         theme(plot.title = element_text(hjust = .5)) + 
-         theme(axis.text.x = element_text(angle = 45, vjust = .5)))
-
-pdf("qa_with_interaction.pdf", w = 5, h = 4)
-print(barplot_interaction)
-dev.off()
